18d89a106c04297210489869827222a1cbae0ef6
Daniel Bogucki <daniel.bogucki14@gmail.com>
2023-01-25 15:13:34 +0100

    final fixes

---
+++ src/utils/texts.ts
@@ -0,0 +1,11 @@
+export const indexTexts = {
+    about: "Zajmuję się fotografią i filmowaniem od ponad  35 lat. Swój pierwszy film zrobiłem jeszcze jako uczeń liceum w latach 80-tych. Od tego też czasu ta dziedzina jest moją pasją. Jako narzędzi używam tylko najwyższej jakości smartfonów. Obecnie jest to iPhone 14 PRO MAX, który pozwala na fantastyczne ujęcia filmowe , panoramiczne i krajobrazowe.",
+    info: "Do filmowania eventów, uroczystości rodzinnych czy filmów reklamowych podchodzę bardzo indywidualnie. Robię szczegółowy wywiad ze zleceniodawcą czego dokładnie oczekuje. W przypadku uroczystości rodzinnych staram się poznać jak najlepiej uczestników nagrań m.in. ich typ charakteru, hobby, przyjaciół, styl życia. Pozwala mi to nagrać i zmontować film „skrojony” na miarę potrzeb i oczekiwań moich Klientów. Zapraszam do Współpracy.",
+    youtube: "Od paru lat umieszczam niektóre swoje produkcje na kanale YOU TUBE. Znajdziecie tu filmy, które robiłem dla tak siebie, jak i Przyjaciół oraz na indywidualne zamówienia i zlecone mi przez firmy reklamowe z szerokiego sektora biznesowego. "
+}
+
+export const pricingTexts = {
+    montage: "Na podstawie przesłanych materiałów i informacji oraz uważnym wywiadem z Klientem proponuje swoją wizje montażu i po jej zaakceptowaniu przystepuję do realizacji.",
+    filming: "Jestem do Waszej dyspozycji  24/7/365.  Wcześniej umawiam się na spotkanie na którym poznaje przede wszystkim Was ale też Wasze oczekiwania, hobby i Wasz styl życia. Na  podstawie tych werbalnych i niewerbalnych informacji analizuje dogłębnie potrzeby Klienta i staram się wyjść im naprzeciw. Cena zawiera całość materiału wideo, wraz z montażem po evencie.",
+    full: "Oprócz zawartych wyżej wymienionych usług - robię 2 wersje filmu do tego 2 extra spoty 2-minutowe , które możecie umieścić na sieciach społecznościowych jako np. zwiastun ostatecznej pełnometrażowej wersji , którą dla Was stworzę."
+}
